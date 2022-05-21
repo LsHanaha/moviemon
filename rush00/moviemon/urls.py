09bb7a9e2a40e087c650d437fe4f892cd62aaf8e
LsHanaha <kirik193@yandex.ru>
2022-05-21 16:00:59 +0200

    make urls

---
+++ rush00/moviemon/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.title_screen, name='title_screen'),
+    path('/worldmap', views.worldmap, name='worldmap'),
+    path('/battle/<int:moviemon_id>', views.battle, name='battle'),
+    path('/moviedex/<int:moviemon>', views.detail, name='detail'),
+    path('/moviedex', views.moviedex, name='moviedex'),
+    path('/options/save_game', views.save, name='save_game'),
+    path('/options/load_game', views.load, name='load_game'),
+    path('/options', views.option, name='options'),
+    # path('', views.title_screen, name='index'),
+]
--- rush00/moviemon/views.py
+++ rush00/moviemon/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def title_screen(request):
+    pass
+
+
+def worldmap(request):
+    pass
+
+
+def battle(request, moviemon_id: int):
+    pass
+
+
+def moviedex(request):
+    pass
+
+
+def detail(request, moviemon: int):
+    pass
+
+
+def option(request):
+    pass
+
+
+def save(request):
+    pass
+
+
+def load(request):
+    pass
