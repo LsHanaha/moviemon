39f1605a90638b1a45c33cb404179dae33a1ce54
mikel45 <ponomarev45@gmail.com>
2022-05-22 15:13:51 +0300

    5 more films

--- rush00/moviemon/imdb_api.py
+++ rush00/moviemon/imdb_api.py
@@ -5,8 +5,8 @@ import json
 def main():
     API_KEY = "4279563e"
     URL = f"http://www.omdbapi.com/?apikey={API_KEY}"
-    FILMS = ["tt8362852", "tt0065163", "tt0120616", "tt0034398", "tt3120314",
-             "tt0367677", "tt1594913", "tt5884052", "tt1572315", "tt0329101"]
+    FILMS = ["tt8362852", "tt0065163", "tt0120616", "tt0034398", "tt3120314", "tt0367677", "tt1594913", "tt5884052",
+             "tt1572315", "tt0329101", "tt0117342", "tt0810743", "tt0105643", "tt0024216", "tt0107290"]
     data = {}
     for film in FILMS:
         params = {
